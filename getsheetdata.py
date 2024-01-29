a704a11d580c641de01af790bc27406b97d86301
jeffiscow2 <jeffiscow2@gmail.com>
2024-01-29 10:05:25 +0000

    how to search data from sheets?

---
+++ getsheetdata.py
@@ -0,0 +1,57 @@
+import os.path
+
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+
+# If modifying these scopes, delete the file token.json.
+SCOPES = ["https://www.googleapis.com/auth/spreadsheets.readonly"]
+
+
+
+
+def get_values(spreadsheet_id, range_name):
+ 
+  creds = None
+  # The file token.json stores the user's access and refresh tokens, and is
+  # created automatically when the authorization flow completes for the first
+  # time.
+  if os.path.exists("token.json"):
+    creds = Credentials.from_authorized_user_file("token.json", SCOPES)
+  # If there are no (valid) credentials available, let the user log in.
+  if not creds or not creds.valid:
+    if creds and creds.expired and creds.refresh_token:
+      creds.refresh(Request())
+    else:
+      flow = InstalledAppFlow.from_client_secrets_file(
+          "credentials.json", SCOPES
+      )
+      creds = flow.run_local_server(port=0)
+    # Save the credentials for the next run
+    with open("token.json", "w") as token:
+      token.write(creds.to_json())
+
+  try:
+    service = build("sheets", "v4", credentials=creds)
+    
+    search_name = input("enter csc username ")
+    
+    result = (
+        service.spreadsheets()
+        .values()
+        .get(spreadsheetId=spreadsheet_id, range=range_name)
+        .execute()
+    )
+ 
+    
+    return result
+  except HttpError as error:
+    print(f"An error occurred: {error}")
+    return error
+
+
+if __name__ == "__main__":
+    # The ID and range of a sample spreadsheet.
+  get_values("1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms","A2:F31")
